8631020deb598aa9fd50da01dacdb2bb22b51b01
ahsanhabib91 <habibibnakther@gmail.com>
2022-05-22 06:30:44 +0600

    Added store procedure bounded execution demo

---
+++ CosmosDb.ServerSide/Server/boundedExecution.js
@@ -0,0 +1,36 @@
+﻿function spBoundedExecution(docs) {
+
+    var context = getContext();
+    var collection = context.getCollection();
+    var response = context.getResponse();
+
+    // initialize count and start recursive call to loop through documents
+    var count = 0;
+    processDocuments(docs[0]);
+
+    function processDocuments(doc) {
+
+        // do some processing over the document
+
+        // insert the document to the container
+        var isAccepted = collection.createDocument(collection.getSelfLink(), doc,
+            // callback function after createDocument completes
+            function (err, doc) {
+                if (err) throw err;
+                count++;
+                if (count == doc.length)
+                    // done; return full count
+                    response.setBody(count);
+                else
+                    // not done; recursive call for next document
+                    processDocuments(docs[count]);
+            }
+        );
+
+        // if wasn't accepted, we're out of time (bounded execution)
+        if (!isAccepted) {
+            // return partial count
+            response.setBody(count);
+        }
+    }
+}
